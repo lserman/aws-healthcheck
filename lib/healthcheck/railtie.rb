84ab5684e6d73c7a9456b489d85aeaedcb4e980d
Logan Serman <logan.serman@metova.com>
2014-10-03 12:46:26 -0500

    Using config.app_middleware in Railtie

--- lib/healthcheck/railtie.rb
+++ lib/healthcheck/railtie.rb
@@ -2,10 +2,6 @@ require 'rails'
 
 module Healthcheck
   class Railtie < ::Rails::Railtie
-
-    initializer 'healthcheck.middleware' do |_|
-      config.middleware.use Healthcheck::Middleware
-    end
-
+      config.app_middleware.use Healthcheck::Middleware
   end
 end
