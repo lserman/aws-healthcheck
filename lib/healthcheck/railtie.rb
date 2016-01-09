97a8042ffef06395047938ef069e3961c83abf5f
Logan Serman <loganserman@gmail.com>
2016-01-08 19:02:09 -0600

    Fix spacing

--- lib/healthcheck/railtie.rb
+++ lib/healthcheck/railtie.rb
@@ -2,6 +2,6 @@ require 'rails'
 
 module Healthcheck
   class Railtie < ::Rails::Railtie
-      config.app_middleware.use Healthcheck::Middleware
+    config.app_middleware.use Healthcheck::Middleware
   end
 end
