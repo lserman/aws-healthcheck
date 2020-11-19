5840e731d8b6194d7baedc66eece9ac210fc9aaa
Logan Serman <logan@serman.dev>
2020-11-19 14:17:33 -0500

    Remove Rails dependency and combustion bloat, add GIT_COMMIT to response if set

--- lib/healthcheck/middleware.rb
+++ lib/healthcheck/middleware.rb
@@ -5,8 +5,16 @@ module Healthcheck
     end
 
     def call(env)
-      return [200, {}, []] if env['PATH_INFO'] == '/healthcheck'
-      @app.call env
+      if env['PATH_INFO'] == '/healthcheck'
+        [ 200, { 'Content-Type' => 'application/json' }, [ body.to_json ] ]
+      else
+        @app.call env
+      end
     end
+
+    private
+      def body
+        { commit: ENV['GIT_COMMIT'].to_s }
+      end
   end
 end
